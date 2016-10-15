764287842a377574884df75cf211739ec3e95594
clinetty <clintogechi@gmail.com>
2016-10-15 18:45:21 +0300

    instance variables

---
+++ class.rb
@@ -0,0 +1,14 @@
+class Person
+	
+	def smile
+		@time=0
+		puts "smile for #{@time} seconds"
+	end
+	def walking
+		puts "walking for #{@time} seconds"
+end
+end
+	carol=Person.new
+	carol.smile
+	carol.walking
+
