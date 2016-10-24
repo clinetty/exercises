98e046821c25f1589024744aa725fa942300ccbc
clinetty <clintogechi@gmail.com>
2016-10-24 18:49:44 +0300

    vehicle_methods.rb

---
+++ vehicle_methods.rb
@@ -0,0 +1,16 @@
+def accelearate
+	puts "Steeping on the gas"
+	puts "Speeding up"
+end
+ def sound_horn
+ 	puts "Pressing the horn button"
+ 	puts "Beep beep"
+ end
+ def use_headlights (brightness)
+ 	puts "Turning on #{brightness} headlights"
+ 	puts "Watch out for deer!"
+ end
+
+ sound_horn
+ accelearate
+ use_headlights ("high-beam")
