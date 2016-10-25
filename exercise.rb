b0a86fbde92da908dcd561d65024356a7284e188
clinetty <clintogechi@gmail.com>
2016-10-25 15:32:15 +0300

    exercise on chapter 3

---
+++ exercise.rb
@@ -0,0 +1,40 @@
+class Robot
+	attr_accessor :name
+	def activate
+		puts "#{@name} is powering up"
+	end
+
+	def move(destination)
+		puts "#{@name} walks to #{destination}"
+		end
+
+	end 
+
+	class TankBot < Robot
+		attr_accessor :weapon
+
+		def attack
+			puts"#{@name} fire #{@weapon}"
+	end
+
+	def move(destination)
+		puts "#{@name} rolls to #{destination}"
+		end
+	end
+
+	class Solar<Robot
+		
+
+		def activate
+			puts" #{@name} deploys solar panel"
+			super
+	end	
+end
+
+tank=TankBot.new
+tank.name="Hugo"
+tank.weapon="laser"
+tank.activate
+tank.move("test dummy")
+tank.attack
+
