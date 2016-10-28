21f455a2323e36a6866d567d6e2a6ba168b4ebcf
clinetty <clintogechi@gmail.com>
2016-10-28 16:42:49 +0300

    find all.rb

---
+++ find_all.rb
@@ -0,0 +1,4 @@
+p [1,2,3,4,5,6].find_all { |number| number.even? }
+p [1,2,3,4,5,6].find_all { |number| number.odd? }		
+p "apple, avocado, anvil".split(", ")
+p ["apple, avocado, anvil"].find_index("anvil")
