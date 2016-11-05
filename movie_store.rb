09d775d575fb63cb5f82cba950343f75788f3dcb
clinetty <clintogechi@gmail.com>
2016-11-05 15:19:15 +0300

    Movie sinatra

---
+++ movie_store.rb
@@ -0,0 +1,14 @@
+require 'yaml/store'
+
+class MovieStore
+
+	def initialize (file_name)
+		@store = YAML::Store.new (file_name)
+	end
+
+	def all
+		@store.transaction do
+		@store.roots.map { |id| @store [id] }
+		end
+	end
+end
